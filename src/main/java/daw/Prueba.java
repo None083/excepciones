1f93c8f6028a24e8f298decc4d72bda4aa40d785
Noelia <perezgonzaleznoelia08@gmail.com>
2022-10-31 14:25:20 +0100

    1

---
+++ src/main/java/daw/Prueba.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package daw;
+
+/**
+ *
+ * @author noelia
+ */
+public class Prueba {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        //metodo main para probar los 4 metodos anteriores
+    }
+    
+}
---
+++ src/main/java/daw/Utilidades.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package daw;
+
+/**
+ *
+ * @author noelia
+ */
+public class Utilidades {
+
+    public static void main(String[] args) {
+        //metodo leer entero sin errores
+        //metodo leer entero en un rango
+        //metodo compararEnteroRango
+        //metodo generar aleatorio rango
+    }
+}
