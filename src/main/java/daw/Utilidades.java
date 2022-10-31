67c67dd7f61977d0827c49e555dc61bf732797fb
Noelia <perezgonzaleznoelia08@gmail.com>
2022-10-31 14:42:49 +0100

    .

--- src/main/java/daw/Utilidades.java
+++ src/main/java/daw/Utilidades.java
@@ -16,5 +16,23 @@ public class Utilidades {
         //metodo leer entero en un rango
         //metodo compararEnteroRango
         //metodo generar aleatorio rango
+        
+        
+        
+        //cosas
+        
+        //recibe tres int, el primero es el numero que quiero comprobrar
+        //el segundo es el rango minimo y el tercero el maximo
+        private static boolean comprobarRango (int numero, int min, int max) {
+            boolean resultado = false;
+            
+            if (numero>=min&&numero<=max) {
+                resultado = true;
+            }
+            
+            return resultado;
+            
+        }
+        
     }
 }
